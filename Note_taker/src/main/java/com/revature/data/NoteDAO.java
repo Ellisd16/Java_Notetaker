e15b130cab4589509a3cf19b6faf2de3c3be4ba3
Ellis Delgado <ellisdelgado13@gmail.com>
2021-09-22 12:32:39 -0700

    Worked on DAOs

--- Note_taker/src/main/java/com/revature/data/NoteDAO.java
+++ Note_taker/src/main/java/com/revature/data/NoteDAO.java
@@ -1,9 +1,18 @@
 package com.revature.data;
 
+import java.util.UUID;
+
+import org.springframework.data.cassandra.repository.Query;
 import org.springframework.data.cassandra.repository.ReactiveCassandraRepository;
 
 import com.revature.dto.NoteDTO;
 
+import reactor.core.publisher.Mono;
+
 public interface NoteDAO extends ReactiveCassandraRepository<NoteDTO, String>{
-//	@Query("Select * from notes")
+	
+	@Query("Select * from Note_taker.notes where title = ?")
+	Mono<NoteDTO> findbyTitle(String title);
+	
+	Mono<NoteDTO> findbyUUID(UUID noteId);
 }
--- Note_taker/src/main/java/com/revature/data/UserDAO.java
+++ Note_taker/src/main/java/com/revature/data/UserDAO.java
@@ -1,5 +1,18 @@
 package com.revature.data;
 
-public interface UserDAO {
 
+
+import org.springframework.data.cassandra.repository.Query;
+import org.springframework.data.cassandra.repository.ReactiveCassandraRepository;
+
+import com.revature.dto.UserDTO;
+
+import reactor.core.publisher.Mono;
+
+public interface UserDAO extends ReactiveCassandraRepository<UserDTO, String>{
+
+	@Query("Select * from Note_taker.users where username = ?")
+	Mono<UserDTO> findbyUsername(String username);
+	
+	
 }
