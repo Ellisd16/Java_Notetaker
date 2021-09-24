495bcd6d431830e206452f46b84c067dd3551269
Ellis Delgado <ellisdelgado13@gmail.com>
2021-09-24 11:58:08 -0700

    Worked on UserController

--- Note_taker/src/main/java/com/revature/controllers/UserController.java
+++ Note_taker/src/main/java/com/revature/controllers/UserController.java
@@ -1,11 +1,21 @@
 package com.revature.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.WebSession;
 
 import com.revature.beans.User;
 import com.revature.service.UserService;
+import com.revature.util.WebSessionAttributes;
+
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/users")
@@ -20,6 +30,27 @@ public class UserController {
 		}
 		
 		//user login
+		@PostMapping("/login")
+		public Mono<ResponseEntity<User>> login(@RequestBody User user, WebSession session){
+			return userSer.login(user.getUsername(), user.getPassword()).map(u -> {
+				session.getAttributes().put(WebSessionAttributes.LOGGED_USER, u);
+				return ResponseEntity.ok(u);
+			}).switchIfEmpty(Mono.just(ResponseEntity.status(401).build()));
+		}
+		
+		@DeleteMapping("/logout")
+		public ResponseEntity<Void> logout(WebSession session){
+			session.invalidate();
+			return ResponseEntity.noContent().build();
+		}
+		@PutMapping(value = "register", produces = MediaType.APPLICATION_JSON_VALUE)
+		public Mono<ResponseEntity<User>> register(@RequestBody User user, WebSession session){
+			return userSer.register(user).map(u -> {
+				session.getAttributes().put(WebSessionAttributes.LOGGED_USER, u);
+				return ResponseEntity.ok(u);
+			}).switchIfEmpty(Mono.just(ResponseEntity.status(404).build()));
+					}
+		
 		
 		
 }
